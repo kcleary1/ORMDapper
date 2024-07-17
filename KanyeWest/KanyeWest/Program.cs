9f53d5950bf5c6adad4829f562473098e89b79e6
Kyle Cleary <kscleary11@gmail.com>
2024-07-17 11:47:20 -0600

    Exercise Complete

---
+++ KanyeWest/KanyeWest/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            for  (int i = 0; i < 5; i++)
+            {
+                KanyeQuote();
+
+                RonQuote();
+            }
+        }
+            
+                public static void KanyeQuote()
+                {
+                var client = new HttpClient();
+
+                var kanyeURL = "https://api.kanye.rest/";
+
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+
+
+                Console.WriteLine($"Kanye: {kanyeQuote}");
+
+                Console.WriteLine("");
+                }
+
+                public static void RonQuote()
+                {
+                var client = new HttpClient();
+
+                var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+                Console.WriteLine($"Ron: {ronQuote}");
+
+                Console.WriteLine("");
+                }
+            
+    }
+        
+    
+}
